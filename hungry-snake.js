53c8b7705922d483d09cd1967e60597502bdf0a3
Geng Shuang <937493108@qq.com>
2017-02-28 00:48:30 +0800

    这次功能算是基本完成了……

--- hungry-snake.js
+++ hungry-snake.js
@@ -1,7 +1,7 @@
 window.onload = function(){
 	var snakeBodyArray = [],// 蛇的肢体列表
-	parts = [],// 蛇分成的几段
-	body = document.body;
+		parts = [],// 蛇分成的几段
+		body = document.body;
 	
 	/**
 	 * 操作食物模块
@@ -29,33 +29,41 @@ window.onload = function(){
 			},
 			isShow: function(){
 				return foodStyle.display === "block" ? true : false;
+			},
+			getFood: function(){
+				return food;
 			}
 		};
 	})(document, body);
 	
 	// 初始化蛇身体
-	for(var i=0;i<4;i++){
-		// 默认 4 节
-		var snakeBody = document.createElement("div");
-		snakeBody.className = "snake";
-		body.appendChild(snakeBody);
-		snakeBodyArray.push(snakeBody);
-		
-		// 默认一段，往左走
-		parts.push({
-			direction: -1,
-			length: 4
-		});
-	}
-
-	snakeBodyArray.reduce(function(previous, current){
-		if(current != snakeBodyArray[0]){
-			current.style.left = (previous.offsetLeft + previous.offsetWidth) + "px";
-		}else{
-			current.style.left = current.offsetLeft + "px";
+	(function(){
+		for(var i=0;i<4;i++){
+			// 默认 4 节
+			var snakeBody = document.createElement("div");
+			snakeBody.style.left = "200px";
+			snakeBody.style.top = "200px";
+			snakeBody.className = "snake";
+			body.appendChild(snakeBody);
+			snakeBodyArray.push(snakeBody);
+			
+			// 默认一段，往左走
+			parts.push({
+				direction: -1,
+				length: 4
+			});
 		}
-		return current;
-	}, snakeBodyArray[0]);
+
+		// 初始化蛇的肢体位置
+		snakeBodyArray.reduce(function(previous, current){
+			if(current != snakeBodyArray[0]){
+				current.style.left = (previous.offsetLeft + previous.offsetWidth) + "px";
+			}else{
+				current.style.left = current.offsetLeft + "px";
+			}
+			return current;
+		}, snakeBodyArray[0]);		
+	})();
 	
 	// 走起
 	setTimeout(function(){
@@ -86,6 +94,7 @@ window.onload = function(){
 		// 把最后一段中的一节设置为蛇头
 		snakeBodyArray.unshift(lastBody);
 		
+		// 第一段由于蛇尾添加到第一个位置，这段的肢体数量 + 1
 		parts[0].length = parts[0].length + 1;
 		
 		// 判断最后一段长度，若为 0，删除之
@@ -95,11 +104,42 @@ window.onload = function(){
 		
 		// 是否吃到食物
 		if(foodModule.isShow()){			
-			// todo
+			if(foodModule.getFood().offsetLeft == lastBody.offsetLeft &&
+			   foodModule.getFood().offsetTop == lastBody.offsetTop){
+				// 隐藏食物
+				foodModule.showFood(false);
+				
+				// 给蛇新增一节
+				var snakeHead = document.createElement("div");
+				snakeHead.style.left = "-1000px";
+				snakeHead.style.top = "-1200px";
+				snakeHead.className = "snake";
+				body.appendChild(snakeHead);
+				snakeBodyArray.push(snakeHead);
+				
+				parts[parts.length-1].length += 1;
+			}
 		}
 		
-		setTimeout(arguments.callee, 1000);
-	}, 1000);
+		// 是否碰壁
+		if(lastBody.offsetLeft <= 0 || lastBody.offsetLeft + lastBody.offsetWidth >= body.clientWidth ||
+		   lastBody.offsetTop <= 0 || lastBody.offsetTop + lastBody.offsetHeight >= body.clientHeight){
+			alert("You're dead! Please restart.");
+			
+		}
+		
+		// 是否咬到自己
+		for(var i=0,len=snakeBodyArray.length;i<len;i++){
+			if(i !== 0){
+				if(snakeBodyArray[i].offsetLeft == lastBody.offsetLeft && 
+				   snakeBodyArray[i].offsetTop == lastBody.offsetTop){
+					alert("You're dead!Please restart.");
+				}
+			}
+		}
+		
+		setTimeout(arguments.callee, 100);
+	}, 100);
 	
 	/**
 	 * 蛇拐弯，添加一段
@@ -135,53 +175,38 @@ window.onload = function(){
 	}
 	
 	/**
-	 * 获取蛇的肢体中 minLeft maxLeft minTop maxTop
-	 */
-	function getSnakeMinAndMaxLeftAndTop(snakeBodyArray){
-		var minAndMaxLeftAndTop = {
-			minLeft: "",
-			maxLeft: "",
-			minTop: "",
-			maxTop: ""
-		};
-		snakeBodyArray.reduce(function(pre, cur){
-			var offsetLeft = cur.offsetLeft,
-				offsetTop = cur.offsetTop,
-				minLeft = pre.minLeft || pre.offsetLeft,
-				maxLeft = pre.maxLeft || pre.offsetLeft,
-				minTop = pre.minTop || pre.offsetTop,
-				maxTop = pre.maxTop || pre.offsetTop;
-			if(minLeft >= offsetLeft){
-				minAndMaxLeftAndTop.minLeft = offsetLeft;
-			}
-			if(maxLeft <= offsetLeft){
-				minAndMaxLeftAndTop.maxLeft = offsetLeft;
-			}
-			if(minTop >= offsetTop){
-				minAndMaxLeftAndTop.minTop = offsetTop;
-			}
-			if(maxTop <= offsetTop){
-				minAndMaxLeftAndTop.maxTop = offsetTop;
-			}
-			return minAndMaxLeftAndTop;
-		}, snakeBodyArray[0]);
-		return minAndMaxLeftAndTop;
-	}
-	
-	/**
 	 * 生成食物的投放位置
 	 * snakeBodyArray 蛇肢体列表
 	 */
 	function generateFoodPoint(snakeBodyArray){
+		var snakeHead = snakeBodyArray[0];
 		// 食物的横坐标
-		var x = parseInt((Math.random() + "").substring(2, 5));
-		// 食物的纵坐标
-		var y = parseInt((Math.random() + "").substring(2, 5));
-		var minAndMaxLeftAndTop = getSnakeMinAndMaxLeftAndTop(snakeBodyArray);
-		if(x >= minAndMaxLeftAndTop.minLeft && x <= minAndMaxLeftAndTop.maxLeft + snakeBodyArray[0].offsetWidth &&
-		   y >= minAndMaxLeftAndTop.minTop && y <= minAndMaxLeftAndTop.maxTop + snakeBodyArray[0].offsetHeight){
-			arguments.callee.call(null, arguments);
+		var x = parseInt((Math.random() + "").substring(2, 4));
+		if(parseInt((Math.random() + "").substring(2, 3)) >= 5){
+			x = -x;
 		}
+		x = snakeHead.offsetWidth * x + snakeHead.offsetLeft;
+		
+		// 食物的纵坐标
+		var y = parseInt((Math.random() + "").substring(2, 4));
+		if(parseInt((Math.random() + "").substring(2, 3)) >= 5){
+			y = -y;
+		}
+		y = snakeHead.offsetHeight * y + snakeHead.offsetTop;
+			
+		// 不能与蛇接触
+		for(var s of snakeBodyArray){
+			if(x == s.offsetLeft && y == s.offsetTop){
+				return arguments.callee.call(null, snakeBodyArray);
+			}
+		}
+		
+		// 不能超出浏览器可视窗口
+		if(x < 0 || x + snakeHead.offsetWidth > body.clientWidth || 
+		   y < 0 || y + snakeHead.offsetHeight > body.clientHeight){
+			return arguments.callee.call(null, snakeBodyArray);
+		}
+		
 		return {
 			x: x,
 			y: y
@@ -190,8 +215,11 @@ window.onload = function(){
 	
 	// 定时投放食物
 	setTimeout(function(){
-		var foodPoint = generateFoodPoint(snakeBodyArray);
-		foodModule.setFoodPosition(foodPoint.x + "px", foodPoint.y + "px");
-		foodModule.showFood(true);
+		if(!foodModule.isShow()){			
+			var foodPoint = generateFoodPoint(snakeBodyArray);
+			foodModule.setFoodPosition(foodPoint.x + "px", foodPoint.y + "px");
+			foodModule.showFood(true);
+		}
+		setTimeout(arguments.callee, 2000);
 	}, 2000);
 };
